cc0d2c50a1b693865a0eadf86a84c5c1cb1d4890
Bratipah <bratipahmh@gmail.com>
2020-09-07 02:57:30 +0300

    get value for order and contact forma

---
+++ script.js
@@ -0,0 +1,17 @@
+//contact forms values
+function subscribe(form1) {
+    var name = document.forms["formcontact"]["name"];
+    var email = document.forms["formcontact"]["email"];
+    var message = document.forms["formcontact"]["message"];
+    alert("We have received your message. " + name + " Thank you for reaching to us");
+};
+
+//orderchoice form value
+function submit(form1) {
+    var pizzasize = document.forms["formorder"]["pizzasize"];
+    var pizzatopping = document.forms["formorder"]["pizzatopping"];
+    var pizzacrust = document.forms["formorder"]["pizzacrust"];
+    var pizzanumber = document.forms["formorder"]["pizzanumber"];
+
+};
+
