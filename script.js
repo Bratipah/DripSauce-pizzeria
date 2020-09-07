1365328c4442b02981161df90698ae53bc2d67a9
Bratipah <bratipahmh@gmail.com>
2020-09-07 03:17:26 +0300

    add js to pages

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 //contact forms values
 function subscribe(formcontact) {
-    var name = document.forms["formcontact"]["name"];
-    var email = document.forms["formcontact"]["email"];
-    var message = document.forms["formcontact"]["message"];
+    var name = document.forms["formcontact"]["name"].value;
+    var email = document.forms["formcontact"]["email"].value;
+    var message = document.forms["formcontact"]["message"].value;
     alert("We have received your message. " + name + " Thank you for reaching to us");
 };
 
 //orderchoice form value
 function submit(formorder) {
-    var pizzasize = document.forms["formorder"]["pizzasize"];
-    var pizzatopping = document.forms["formorder"]["pizzatopping"];
-    var pizzacrust = document.forms["formorder"]["pizzacrust"];
-    var pizzanumber = document.forms["formorder"]["pizzanumber"];
+    var pizzasize = document.forms["formorder"]["pizzasize"].value;
+    var pizzatopping = document.forms["formorder"]["pizzatopping"].value;
+    var pizzacrust = document.forms["formorder"]["pizzacrust"].value;
+    var pizzanumber = document.forms["formorder"]["pizzanumber"].value;
 
+    if (pizzasize === "small") {
+        alert("Small size = KSH800");
+    } else if (pizzasize === "large") {
+        alert("Small size = KSH1000");
+    } else if (pizzasize === "small") {
+        alert("Small size = KSH1500");
+    } else {
+        alert("Choose a size ")
+    }
 };
 
